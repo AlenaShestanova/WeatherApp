aa00d79501e40e9d9b50be95cc6f4a0a8da62e40
AlenaShestanova <alenaneew@mail.ru>
2020-05-25 17:36:22 +0300

    вывод данных о погоде на страницу

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Header from './Components/Header'
 
 import Form from "./Components/Form";
 import Weather from "./Components/Weather";
 const API_KEY='44f3279a58667d6583e74c55bac5aee0'
 function App() {
+    const [state,setState]=React.useState({
+        temp:undefined,
+        city:undefined,
+        country:undefined,
+        sunrise:undefined,
+        sunset:undefined,
+        error:undefined
 
+    })
 async function gettingWeather(event){
 
       event.preventDefault()
@@ -14,12 +22,26 @@ const api_url=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ci
 
       const data= await api_url.json()
       console.log(data)
+    setState({
+        temp:data.main.temp,
+        city:data.name,
+        country:data.sys.country,
+        sunrise:data.sys.sunrise,
+        sunset:data.sys.sunset,
+        error:''
+    })
     }
   return (
       <div>
      <Header></Header>
         <Form weatherMethod={gettingWeather}></Form>
-    <Weather></Weather>
+    <Weather temp={state.temp}
+             city={state.city}
+              county={state.country}
+             sunrise={state.sunrise}
+             sunset={state.sunset}
+             error={state.error}
+    ></Weather>
       </div>
 
   );
--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
-export default function Weather() {
-    return(
-        <div>
-            Компонент с погодой
+export default function Weather(props) {
 
+    return (
+
+        <div>
+            {props.city&&
+            <div>
+                <p>Местоположение:{props.city},{props.county}</p>
+                <p>Температура:{props.temp}</p>
+                <p>Восход солнца:{props.sunrise}</p>
+                <p>Заход солнца:{props.sunset}</p>
+
+
+            </div>
+            }
         </div>
     )
 }
