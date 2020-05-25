398a9541e73723ff44046041f0544fa5841a37bb
AlenaShestanova <alenaneew@mail.ru>
2020-05-25 18:10:34 +0300

    вывод ошибки, если город не введен

--- src/App.js
+++ src/App.js
@@ -1,50 +1,74 @@
-import React,{useState} from 'react';
+import React, {useState} from 'react';
 import Header from './Components/Header'
 
 import Form from "./Components/Form";
 import Weather from "./Components/Weather";
-const API_KEY='44f3279a58667d6583e74c55bac5aee0'
+
+const API_KEY = '44f3279a58667d6583e74c55bac5aee0'
+
 function App() {
-    const [state,setState]=React.useState({
-        temp:undefined,
-        city:undefined,
-        country:undefined,
-        sunrise:undefined,
-        sunset:undefined,
-        error:undefined
+    const [state, setState] = React.useState({
+        temp: undefined,
+        city: undefined,
+        country: undefined,
+        sunrise: undefined,
+        sunset: undefined,
+        error: undefined
 
     })
-async function gettingWeather(event){
 
-      event.preventDefault()
-    const city=event.target.elements.city.value
-const api_url=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=44f3279a58667d6583e74c55bac5aee0`);
+    async function gettingWeather(event) {
 
-      const data= await api_url.json()
-      console.log(data)
-    setState({
-        temp:data.main.temp,
-        city:data.name,
-        country:data.sys.country,
-        sunrise:data.sys.sunrise,
-        sunset:data.sys.sunset,
-        error:''
-    })
+        event.preventDefault()
+        const city = event.target.elements.city.value
+
+        if (city) {
+            const api_url = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=44f3279a58667d6583e74c55bac5aee0`);
+
+            const data = await api_url.json()
+
+            function ConvertTime(time) {
+                var date = new Date()
+                date.setTime(time)
+                return date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
+            }
+
+
+            setState({
+                temp: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                sunrise: ConvertTime(data.sys.sunrise),
+                sunset: ConvertTime(data.sys.sunset),
+                error: undefined
+            })
+        }
+        else{
+            setState({
+                temp: undefined,
+                city: undefined,
+                country: undefined,
+                sunrise: undefined,
+                sunset: undefined,
+                error: 'Введите название города'
+            })
+        }
     }
-  return (
-      <div>
-     <Header></Header>
-        <Form weatherMethod={gettingWeather}></Form>
-    <Weather temp={state.temp}
-             city={state.city}
-              county={state.country}
-             sunrise={state.sunrise}
-             sunset={state.sunset}
-             error={state.error}
-    ></Weather>
-      </div>
 
-  );
+    return (
+        <div>
+            <Header></Header>
+            <Form weatherMethod={gettingWeather}></Form>
+            <Weather temp={state.temp}
+                     city={state.city}
+                     county={state.country}
+                     sunrise={state.sunrise}
+                     sunset={state.sunset}
+                     error={state.error}
+            ></Weather>
+        </div>
+
+    );
 
 }
 
--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -15,6 +15,7 @@ export default function Weather(props) {
 
             </div>
             }
+            <p>{props.error}</p>
         </div>
     )
 }
