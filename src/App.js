17c57aec663789ea3f233d5495724084f02e5389
AlenaShestanova <alenaneew@mail.ru>
2020-05-25 16:59:14 +0300

    получаем погоду по введенному городу

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import Form from "./Components/Form";
 import Weather from "./Components/Weather";
 const API_KEY='44f3279a58667d6583e74c55bac5aee0'
 function App() {
+
 async function gettingWeather(event){
+
       event.preventDefault()
-const api_url=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Kazan&appid=44f3279a58667d6583e74c55bac5aee0`);
-   // const api_url=await fetch('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02')
+    const city=event.target.elements.city.value
+const api_url=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=44f3279a58667d6583e74c55bac5aee0`);
+
       const data= await api_url.json()
       console.log(data)
     }
